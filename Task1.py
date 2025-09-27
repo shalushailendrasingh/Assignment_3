7cdfed072b56056bb22150a2990b5f0cb16fbb54
Shalini Singh <151626296+shalushailendrasingh@users.noreply.github.com>
2025-09-27 16:55:27 +0530

    Add files via upload
    Problem Statement
    
    Write a Python program that:
    Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
    Returns the calculated factorial.
    Calls the function with a sample number and prints the output.

---
+++ Task1.py
@@ -0,0 +1,11 @@
+# Task 1:Calculate Factorial Using a Function
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = int(input("Enter a number: "))
+
+print(f"The factorial of {num} is: {factorial(num)}")
